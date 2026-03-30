9feab0d46e8a917f24715d1f97b6f55ed6c6fb04
cchikong <cchikong@gmail.com>
2026-03-30 23:31:08 +0800

    3.0v

--- src/ClassA.java
+++ src/ClassA.java
@@ -11,6 +11,8 @@ public class ClassA {
     public static void printMessage() {
         System.out.println("Hello from ClassA! - V2.3");
         System.out.println("2.7");
+        System.out.println("3.0v");
+
     }
     
 }
